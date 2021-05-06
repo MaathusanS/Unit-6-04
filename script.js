3a019b8889ae36308ef75a56c9cd1d22e0f7a574
MaathusanS <>
2021-05-06 14:48:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let A = 0 // Defines A as variable.
+let B = 0 // Defines B as variable.
+let H = 0 // Defines H as variable.
+
+document.body.style.backgroundColor = '#ffddd2' // Sets the document body colour.
+document.getElementById('calc').addEventListener('click', area) // Creates an event listener for calculate button, which then triggers the area function.
+
+function area () {
+  A = document.getElementById('abase').value // Takes the user's input from the A base box.
+  B = document.getElementById('bbase').value // Takes the user's input from the B base box.
+  H = document.getElementById('height').value // Takes the user's input from the height box.
+  A = parseFloat(A) // Turns the inputted string into a float.
+  B = parseFloat(B) // Turns the inputted string into a float.
+  H = parseFloat(H) // Turns the inputted strng into a float.
+  area = (A+B)/2*H // Performs the area calculation.
+  document.getElementById('number').innerHTML = 'Your area is ' + area + ' units squared!' // Prints the area out to the user on the page.
+  if (isNaN(area)) {
+    alert('Not a valid number!') // If a valid number isn't given, the user will be faced with an alert that says the number is not valid.
+  }
+}
