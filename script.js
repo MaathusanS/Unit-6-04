481abe83c8b58b0b173704679f1119996b2e4c99
MaathusanS <>
2021-05-06 14:55:34 +0000

    Fixed spacing and syntax

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function area () {
   A = parseFloat(A) // Turns the inputted string into a float.
   B = parseFloat(B) // Turns the inputted string into a float.
   H = parseFloat(H) // Turns the inputted strng into a float.
-  area = (A+B)/2*H // Performs the area calculation.
-  document.getElementById('number').innerHTML = 'Your area is ' + area + ' units squared!' // Prints the area out to the user on the page.
-  if (isNaN(area)) {
+  area1 = (A + B) / 2 * H // Performs the area calculation.
+  document.getElementById('number').innerHTML = 'Your area is ' + area1 + ' units squared!' // Prints the area out to the user on the page.
+  if (isNaN(area1)) {
     alert('Not a valid number!') // If a valid number isn't given, the user will be faced with an alert that says the number is not valid.
   }
 }
